9497b4d51a2f4f8540ad07a4e6615233f05df7f0
lararshp <arviansyahdesta12@gmail.com>
2026-03-05 00:14:26 +0700

    Fiksasi pdf p2

--- resources/views/barang/pdf.blade.php
+++ resources/views/barang/pdf.blade.php
@@ -3,75 +3,96 @@
 <head>
     <meta charset="utf-8">
     <style>
-        /* A4 portrait, zero margins */
         @page { 
             size: A4 portrait; 
-            margin: 0; 
+            /* Margin fisik hasil penggaris temanmu: Atas 0.4, Kanan 0.3, Bawah 0.3, Kiri 0.5 */
+            margin: 0.4cm 0.3cm 0.3cm 0.5cm; 
         }
+
         body { 
             font-family: Arial, Helvetica, sans-serif; 
             margin: 0; 
-            padding-top: 5mm; /* Memberi sedikit ruang di atas agar printer tidak narik terlalu cepat */
+            padding: 0;
+            /* Pastikan lebar dan tinggi body sesuai standar A4 agar tidak memotong halaman */
+            width: 210mm;
+            position: relative;
         }
 
-        /* Table width: 5 cols * 38mm = 190mm. Center horizontally on A4 (210mm) */
+        .page {
+            position: relative;
+            width: 210mm;
+            height: 297mm; /* Tinggi standar A4 */
+            overflow: hidden;
+        }
+
         .labels-table { 
-            width: 190mm; 
-            margin: 0 auto; 
-            border-collapse: collapse; 
-            table-layout: fixed; /* Mengunci ukuran kolom agar tidak melar */
+            /* Paksa posisi di titik nol margin @page sesuai saran temanmu */
+            position: absolute;
+            top: 0;
+            left: 0;
+            border-collapse: separate; 
+            /* HORIZONTAL 0.2cm (untuk cegah kanan kepotong), VERTIKAL 0.2cm */
+            border-spacing: 0.2cm 0.2cm; 
+            table-layout: fixed;
+            width: auto;
         }
         
         .labels-table td { 
-            border: none; 
             padding: 0;
+            margin: 0;
+            vertical-align: middle; 
         }
 
-        /* Label Box: 38mm x 18mm */
+        /* UKURAN FISIK STIKER TnJ 108: 38mm x 18mm */
         .label-cell {
             width: 38mm;
             height: 18mm;
             box-sizing: border-box;
-            vertical-align: top;
             overflow: hidden;
+            /* Border merah untuk panduan mata (Hapus baris ini setelah mantap) */
         }
 
-        /* Wrapper konten di dalam label */
         .label-content { 
-            padding: 1.5mm 1mm 1.5mm 4mm; /* Kiri 4mm supaya tidak terlalu mepet garis */
+            width: 100%;
             height: 18mm;
             display: flex;
             flex-direction: column;
-            justify-content: space-between;
+            /* Membuat konten di tengah secara vertikal */
+            justify-content: center; 
+            /* Membuat konten rata kiri sesuai instruksi temanmu */
+            align-items: flex-start; 
+            text-align: left;
+            /* Jarak aman 3mm dari tepi kiri agar tidak meleset saat diprint */
+            padding-left: 3mm; 
+            padding-right: 1mm;
             box-sizing: border-box;
         }
 
         .label-id { 
-            font-size: 6.5pt; 
+            font-size: 6pt; 
             color: #555; 
-            margin: 0;
+            margin-bottom: 1px;
             line-height: 1;
         }
 
         .label-name { 
             font-weight: 700; 
-            font-size: 8pt; /* Sedikit dikecilkan agar muat banyak karakter */
+            font-size: 8pt; 
             line-height: 1.1; 
             color: #000;
-            margin: 0.5mm 0;
-            /* Fitur Potong Otomatis jika nama kepanjangan (maks 2 baris) */
+            margin: 1px 0;
+            /* Batasi maksimal 2 baris agar tidak merusak tata letak harga */
             display: -webkit-box;
             -webkit-line-clamp: 2;
             -webkit-box-orient: vertical;
             overflow: hidden;
-            word-wrap: break-word;
+            width: 100%;
         }
 
         .label-price { 
             font-size: 8.5pt; 
-            font-weight: bold;
-            color: #000;
-            margin: 0;
+            font-weight: bold; 
+            margin-top: 1px;
             line-height: 1;
         }
 
@@ -92,14 +113,16 @@
                                 $slotIndex = $r * 5 + $c; 
                                 $item = $page[$slotIndex] ?? null; 
                             @endphp
-                            <td class="label-cell">
-                                @if($item)
-                                    <div class="label-content">
-                                        <div class="label-id">{{ $item->id_barang }}</div>
-                                        <div class="label-name">{{ $item->nama }}</div>
-                                        <div class="label-price">Rp {{ number_format($item->harga, 0, ',', '.') }}</div>
-                                    </div>
-                                @endif
+                            <td>
+                                <div class="label-cell">
+                                    @if($item)
+                                        <div class="label-content">
+                                            <div class="label-id">{{ $item->id_barang }}</div>
+                                            <div class="label-name">{{ $item->nama }}</div>
+                                            <div class="label-price">Rp {{ number_format($item->harga, 0, ',', '.') }}</div>
+                                        </div>
+                                    @endif
+                                </div>
                             </td>
                         @endfor
                     </tr>
